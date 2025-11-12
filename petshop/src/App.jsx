229714fc4d749bc0ed78b8fd9cc6a3dfb27c5418
Cassio <cassiomacielgomes.devops@gmail.com>
2025-11-12 12:03:41 -0300

    Atualização | 12-11-2025

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,15 +1,23 @@
 //import './App.css'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
+import Pagina404 from './pages/404'
 import './assets/css/base/base.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './assets/css/404.css'
+import Cabecalho from './components/cabecalho'
 
 function App() {
-
-  return (
-    <>
-      <Home />
-      <Sobre />
-    </>
+  return(
+    <Router>
+      <Cabecalho />
+      <Routes>
+        {/* Use element={Componente /} para renderizar */}
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='*' element={<Pagina404/>}/>
+      </Routes>
+    </Router>
   )
 }
 
