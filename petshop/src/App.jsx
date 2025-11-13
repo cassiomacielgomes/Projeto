11ece7d82a8035331eacd2d7abbc28db526c133f
Cassio <cassiomacielgomes.devops@gmail.com>
2025-11-13 09:01:22 -0300

    Finalização | 12-11-2025

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -6,6 +6,12 @@ import './assets/css/base/base.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './assets/css/404.css'
 import Cabecalho from './components/cabecalho'
+import Post from './pages/Post'
+import './assets/css/post.css'
+import Categoria from './pages/Categoria'
+import SubCategoria from './pages/SubCategorias'
+import CategoriaPosts from './pages/CategoriaPost'
+
 
 function App() {
   return(
@@ -14,6 +20,25 @@ function App() {
       <Routes>
         {/* Use element={Componente /} para renderizar */}
         <Route path='/' element={<Home/>}/>
+        <Route path='/posts/:id' element={<Post/>}/>
+
+        {/* 
+          Esta é a rota pai. Ela renderiza o <Categoria />
+        */}
+        <Route path='/categorias/:id' element={<Categoria/>}>
+          {/* Esta é a rota "filho" de índice (index).
+              Ela é renderizada dentro do <Outlet> quando a URL
+              é exatamente /categoria/:id 
+            */}
+            <Route index element={<CategoriaPosts />} />
+            
+            {/* Esta é a rota "filho" da subcategoria.
+              Ela é renderizada dentro do <Outlet> quando a URL
+              é /categoria/:id/:subcategoria 
+            */}
+            <Route path=':subcategoria' element={<SubCategoria />} />
+        </ Route>
+
         <Route path='/sobre' element={<Sobre/>}/>
         <Route path='*' element={<Pagina404/>}/>
       </Routes>
